3b0a2153722c993c6a9587f6a8b63a28680cb39c
p-wtt <parksangmin.wyatt@gmail.com>
2021-06-17 08:44:58 +0000

    [2021-06-17] 1 commit

--- gardenerScript.sh
+++ gardenerScript.sh
@@ -10,6 +10,14 @@ index=0
 
 # step 3.. to work as a gardener
 while [ $index -lt $random_number ]
-do`
+do
   index=`expr $index + 1`
+  echo "[$date] The $index st misstion of the gardener" >> ./gardenerLog.txt
+
+  git add . 
+  git commit -m "[$date] $index commit"
+  git push
 done
+
+# step 4..
+echo "Gardener mission terminated"
