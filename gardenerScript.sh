21ed93e2928df788a64ebaa431f8dc6134c62f8d
p-wtt <parksangmin.wyatt@gmail.com>
2021-06-17 08:51:15 +0000

    [2021-06-17] 1commit

--- gardenerScript.sh
+++ gardenerScript.sh
@@ -1,23 +1,23 @@
 #!/bin/bash --login
 
 # step 1.. latest repository
-#git pull origin main
+git pull origin main
 
 # step 2.. create random number
 date=$(date "+%Y-%m-%d")
 random_number=`shuf -i 1-3 -n 1`
 index=0
 
-# step 3.. to work as a gardener
+# step 3.. work as a gardener
 while [ $index -lt $random_number ]
 do
   index=`expr $index + 1`
-  echo "[$date] The $index st misstion of the gardener" >> ./gardenerLog.txt
+  echo "[$date] The ${index}st misstion of the gardener" >> ./gardenerLog.txt
 
   git add . 
-  git commit -m "[$date] $index commit"
+  git commit -m "[$date] ${index}commit"
   git push
 done
 
-# step 4..
-echo "Gardener mission terminated"
+# step 4..end of gardener work
+echo "Gardener mission terminated" >> ./gardenerLog.txt
